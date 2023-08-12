6bafdacd09a672ce2ef65f53d5d71477fd0b9865
Pandey <vishal.pandey@sap.com>
2023-08-12 12:09:43 +0530

    removed redundancy from the code

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 # deploy postgres application
-kubectl apply -f kubernetes/postgres/deployment-kub.yaml
-kubectl apply -f kubernetes/postgres/services-kub.yaml
+kubectl apply -f kubernetes/postgres.yaml
 
 # deploy postgres application
-kubectl apply -f kubernetes/redis/deployment-kub.yaml
-kubectl apply -f kubernetes/redis/services-kub.yaml
+kubectl apply -f kubernetes/redis.yaml
 
 #deploy backend application
-kubectl apply -f kubernetes/backend-app/deployment-kub.yaml
-kubectl apply -f kubernetes/backend-app/services-kub.yaml
+kubectl apply -f kubernetes/backend.yaml
